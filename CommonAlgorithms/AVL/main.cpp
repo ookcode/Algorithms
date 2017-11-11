f68023239933b101c498c22e057f207a81fe0752
Vincent Yao <ookcode@gmail.com>
2017-11-11 10:52:18 +0800

    AVL

--- CommonAlgorithms/AVL/main.cpp
+++ CommonAlgorithms/AVL/main.cpp
@@ -9,33 +9,14 @@
 #include <iostream>
 #include <math.h>
 #include <vector>
+
 struct BinaryNode {
     BinaryNode(int weight):data(weight){};
     int data;
     int bf = 0; // 平衡因子（左子树深度 - 右子树深度）
     BinaryNode* lchild = nullptr;
     BinaryNode* rchild = nullptr;
-    
-    int offset = 0; // 空格数，仅用于格式化输出
 };
-
-int Deep = 0; // 二叉树的层数
-// 二叉排序树插入
-void insert(BinaryNode *&node, int data, int deep = 0) {
-    deep++;
-    if (node == nullptr) {
-        node = new BinaryNode(data);
-        if (deep > Deep) {
-            Deep = deep;
-        }
-        return;
-    }
-    if (data < node->data) {
-        insert(node->lchild, data, deep);
-    } else {
-        insert(node->rchild, data, deep);
-    }
-}
 
 // 平衡二叉树右旋
 void rotateRight(BinaryNode *&root) {
@@ -68,14 +49,17 @@ void balanceLeft(BinaryNode *&root) {
             BinaryNode *Lr = L->rchild;
             switch (Lr->bf) {
                 case -1:
+                    // 情形三
                     root->bf = 0;
                     L->bf = 1;
                     break;
                 case 0:
+                    // 情形二
                     root->bf = 0;
                     L->bf = 0;
                     break;
                 case 1:
+                    // 情形四
                     root->bf = -1;
                     L->bf = 0;
                     break;
@@ -87,7 +71,7 @@ void balanceLeft(BinaryNode *&root) {
         }
         case 1:
         {
-            // 正常右旋
+            // 情形一
             root->bf = 0;
             L->bf = 0;
             rotateRight(root);
@@ -124,7 +108,6 @@ void balanceRight(BinaryNode *&root) {
         }
         case -1:
         {
-            // 正常左旋
             root->bf = 0;
             R->bf = 0;
             rotateLeft(root);
@@ -133,11 +116,25 @@ void balanceRight(BinaryNode *&root) {
     }
 }
 
+// 二叉排序树插入
+void insert(BinaryNode *&node, int data) {
+    if (node == nullptr) {
+        node = new BinaryNode(data);
+        return;
+    }
+    if (data < node->data) {
+        insert(node->lchild, data);
+    } else {
+        insert(node->rchild, data);
+    }
+}
+
 // 平衡二叉树插入
 bool insertAVL(BinaryNode *&node, int data, bool *taller) {
     if (node == nullptr) {
         node = new BinaryNode(data);
         *taller = true;
+        return true;
     } else if (data < node->data) {
         if(!insertAVL(node->lchild, data, taller)) {
             return false;
@@ -183,51 +180,6 @@ bool insertAVL(BinaryNode *&node, int data, bool *taller) {
     return true;
 }
 
-// 层序遍历
-void floorOrderTraverse(BinaryNode *node, int deep) {
-    std::vector<BinaryNode *> line[deep];
-    // 添加根节点
-    node->offset = 70;
-    line[0].push_back(node);
-    // 添加子节点
-    for (int i = 0; i < deep; ++i) {
-        for (int j = 0; j < line[i].size(); ++j) {
-            BinaryNode *node = line[i][j];
-            BinaryNode *&lchild = node->lchild;
-            BinaryNode *&rchild = node->rchild;
-            if (lchild == nullptr) {
-                lchild = new BinaryNode(0);
-            }
-            if (rchild == nullptr) {
-                rchild = new BinaryNode(0);
-            }
-            lchild->offset = node->offset - (deep - i);
-            rchild->offset = node->offset + (deep - i);
-            line[i + 1].push_back(lchild);
-            line[i + 1].push_back(rchild);
-        }
-    }
-    // 格式化输出
-    for (int i = 0; i < deep; ++i) {
-        int size = (int)line[i].size();
-        int offset = 0;
-        for (int j = 0; j < size; ++j) {
-            BinaryNode *node = line[i][j];
-            int nodeOffset = node->offset;
-            printf("");
-            for (; offset < nodeOffset; ++offset) {
-                printf(" ");
-            }
-            if (node->data == 0) {
-                printf("");
-            } else {
-                printf("%d", node->data);
-            }
-        }
-        printf("\n");
-    }
-}
-
 int main(int argc, const char * argv[]) {
     int datas[10] = {3,2,1,4,5,6,7,10,9,8};
     bool status = false;
@@ -235,6 +187,6 @@ int main(int argc, const char * argv[]) {
     for (int i = 1; i < 10; ++i) {
         insertAVL(tree, datas[i], &status);
     }
-    floorOrderTraverse(tree, Deep);
+    
     return 0;
 }
